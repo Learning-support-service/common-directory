4f6d6c9cb993e58e60f72053fa99018730fe7681
ohsj <dhtmdwns319@naver.com>
2025-10-04 21:35:06 +0900

    First commit: routing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import LandingPage from "./pages/SelectCourse/LandingPage";
+import DashBoard from './components/layout/DashBoard/DashBoard';
+import LoginPage from './pages/Login/LoginPage';
+import SignInPage from './pages/SignIn/SignInPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DashBoard />}>
+          <Route index element={<LandingPage />} />
+          <Route path="auth">
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signIn" element={<SignInPage />} />
+          </Route>
+          <Route path="/home" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/SelectCourse/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Landing from '../../components/SelectCourse/Landing'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
